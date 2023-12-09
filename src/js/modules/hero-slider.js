55a40434cdf326b519ec5d035ea53449afdf2fa4
accura7e <accura7ee@gmail.com>
2023-12-09 23:37:00 +0300

    add: added swiper

---
+++ src/js/modules/hero-slider.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+
+function heroSlider(){
+    const swiper = new Swiper('.hero-swiper', {
+        // Optional parameters
+
+        loop: true,
+        speed: 1500,
+        parallax:true,
+        grabCursor: true,
+        // If we need pagination
+        pagination: {
+          el: '.hero__slider-pagination',
+          renderBullet: function (index, className) {
+            return `<span class="${className}">
+                      <span class="pagination-num">0${(index + 1)}</span>
+                    </span>`
+          },
+        },
+
+        // 
+        
+        
+
+        // Navigation arrows
+        navigation: {
+          nextEl: '.hero__slider-btn-next',
+          prevEl: '.hero__slider-btn-prev',
+        },
+      
+        // And if we need scrollbar
+
+      });
+}
+
+export default heroSlider
